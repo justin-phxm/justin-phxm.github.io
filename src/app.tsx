2e1604a69ceb8cf823fba052f7db59d370321ddf
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-09-08 17:26:15 -0600

    first init

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import { useState } from "preact/hooks";
+import "./app.css";
+
+export function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline bg-green-300">Hello world!</h1>
+    </>
+  );
+}
