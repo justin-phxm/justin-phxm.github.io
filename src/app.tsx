6e80ee3ae37e63879c58180ddc7164aa9da0a6d1
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-09-08 18:23:17 -0600

    begin navBar

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,37 @@
-import { useState } from "preact/hooks";
-import "./app.css";
+import NavBar from "./NavBar.tsx";
 
 export function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline bg-green-300">Hello world!</h1>
+      <NavBar />
+      {/* <div class="w-96 h-96 left-[-15px] top-[162px] absolute"></div>
+      <div class="w-36 left-[42px] top-[167px] absolute text-slate-200 text-lg">
+        Hi all. I am
+      </div>
+      <div class="w-72 left-[42px] top-[330px] absolute text-emerald-400 text-xl">
+        {" "}
+        Full-stack developer
+      </div>
+      <div class="w-72 left-[42px] top-[201px] absolute text-slate-200 text-6xl font-normal leading-10">
+        Justin Pham
+      </div>
+      <div class="w-64 left-[42px] top-[551px] absolute text-slate-500 text-sm leading-tight">
+        // find my profile on Github:
+      </div>
+      <div class="left-[33px] top-[32px] absolute text-slate-500 text-base">
+        justin-phxm
+      </div>
+      <div class="w-72 left-[42px] top-[587px] absolute">
+        <span style="text-indigo-600 text-base font-medium">const</span>
+        <span style="text-white text-base font-medium"> </span>
+        <span style="text-emerald-400 text-base font-medium">githubLink</span>
+        <span style="text-white text-base font-medium"> = </span>
+        <span style="text-rose-400 text-base font-medium">“</span>
+        <span style="text-rose-400 text-base font-medium underline">
+          https://github.com/justin-phxm
+        </span>
+        <span style="text-rose-400 text-base font-medium">”</span>
+      </div> */}
     </>
   );
 }
