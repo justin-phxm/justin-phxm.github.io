4ecca0e0bfb7c303df1e0edceaf051e1443ce033
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-09-13 10:32:11 -0600

    add footer icons

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,8 +1,20 @@
 import devInfo from "../devInfo.json";
-
+import { AiFillGithub } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { JSX } from "preact/jsx-runtime";
 export default function Footer() {
-  const social = devInfo.contacts.social;
-  console.log(social);
+  type socialType = {
+    [key: string]: {
+      title: string;
+      url: string;
+      icon?: JSX.Element;
+    };
+  };
+  const social: socialType = devInfo.contacts.social;
+  social.github.icon = <AiFillGithub />;
+  social.facebook.icon = <BsFacebook />;
+  social.twitter.icon = <AiFillTwitterCircle />;
   return (
     <>
       <footer class="rounded-lg shadow m-4 text-slate-500 dark:bg-gray-800">
@@ -14,15 +26,10 @@ export default function Footer() {
             {Object.values(social).map((item, index) => (
               <li key={index}>
                 <a href={item.url} className="hover:underline">
-                  {item.title}
+                  {item.icon ? item.icon : item.title}
                 </a>
               </li>
             ))}
-            <li>
-              <a href="#" class="hover:underline">
-                Contact
-              </a>
-            </li>
           </ul>
         </div>
       </footer>
