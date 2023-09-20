6831d6c342757c96b5636c05e51846b7c22a4fcc
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-09-20 01:11:39 -0600

    added decorators

--- src/app.tsx
+++ src/app.tsx
@@ -2,15 +2,17 @@ import NavBar from "./NavBar.tsx";
 import Footer from "./Footer.tsx";
 import Game from "./Game.tsx";
 import devInfo from "../devInfo.json";
+import { RxCrossCircled } from "react-icons/rx";
 export function App() {
   return (
     <>
       <NavBar />
       <div className="flex flex-row">
-        <section class="w-full p-20 ">
+        <section class="w-full p-20 xl:p-28 ">
           <p class="text-slate-200 text-lg">Welcome. I am</p>
           <p class="text-slate-200 text-6xl">{devInfo.name}</p>
           <p class="text-indigo-600 text-3xl">&gt; Full-stack web developer</p>
+          <div className=" py-4"></div>
           <p class="text-slate-500 text-base ">
             // complete the game to continue
           </p>
@@ -35,11 +37,23 @@ export function App() {
             <span class="text-rose-400 text-base font-medium ">”</span>
           </div>
         </section>
-        <section class="hidden w-full md:flex-row md:flex relative">
-          <div class=" bg-emerald-400 w-96 h-96 absolute top-0 left-0 z-0 transform rotate-45 opacity-40 shadow-2xl filter blur-2xl " />
-          <div class="bg-indigo-600 w-96 h-96 absolute top-48 left-28 z-0 transform rotate-12 opacity-40 shadow-2xl filter blur-2xl"></div>
-          <div class=" p-4 bg-gradient-to-br from-teal-900 to-emerald-400 rounded-lg shadow-inner border border-neutral-900 backdrop-blur-3xl flex flex-row w-full">
+        <section class="hidden w-full lg:flex-row lg:flex relative px-20 py-8 xl:px-28 xl:py-20">
+          <div class=" bg-emerald-400 xl:w-96 w-72 xl:h-96 h-72 absolute -top-24 -left-16 z-0 transform rotate-45 opacity-40 shadow-2xl filter blur-2xl " />
+          <div class="bg-indigo-600 xl:w-96 w-72 xl:h-96 h-72 absolute -bottom-32 -right-24 z-0 transform rotate-12 opacity-40 shadow-2xl filter blur-2xl"></div>
+          <div class=" p-4 bg-gradient-to-br from-teal-900 via-transparent to-indigo-950 rounded-lg shadow-inner border border-neutral-900 backdrop-blur-3xl flex flex-row w-full ">
             <Game />
+            <div class="w-4 h-4 left-1 top-1 absolute bg-teal-700 rounded-full shadow-inner">
+              <RxCrossCircled />
+            </div>
+            <div class="w-4 h-4 right-1 top-1 absolute bg-slate-950 rounded-full shadow-inner">
+              <RxCrossCircled />
+            </div>
+            <div class="w-4 h-4 left-1 bottom-1 absolute bg-slate-950 rounded-full shadow-inner">
+              <RxCrossCircled />
+            </div>
+            <div class="w-4 h-4 right-1 bottom-1 absolute bg-blue-950 rounded-full shadow ">
+              <RxCrossCircled />
+            </div>
           </div>
         </section>
       </div>
