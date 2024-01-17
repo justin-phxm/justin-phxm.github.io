4d9c09120e06c839b6f28e913412d78057ee4b52
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-01-17 12:17:05 -0700

    Remove unnecessary code in Contact component

--- src/_contact-me/Contact.tsx
+++ src/_contact-me/Contact.tsx
@@ -5,9 +5,6 @@ export default function Contact({}: { path: string }) {
   return (
     <>
       <div class="flex flex-col lg:flex-row h-full w-full">
-        <div className="text-white sm:hidden py-4 px-3 text-sm">
-          _contact-me
-        </div>
         <SideBar />
         <ContactForm />
       </div>
