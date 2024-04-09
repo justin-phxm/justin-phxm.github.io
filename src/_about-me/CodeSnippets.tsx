0f7c1228a24dd1acd269d81108ca7c32ae66c988
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-04-09 11:58:23 -0600

    update code snippets sizing

--- src/_about-me/CodeSnippets.tsx
+++ src/_about-me/CodeSnippets.tsx
@@ -68,23 +68,21 @@ export default function CodeSnippets() {
     <>
       <div data-aos="fade-left" className=" flex flex-col  ">
         <div className=" inline-block border-b border-slate-800 p-5 w-full" />
-        <div className="p-4 w-full max-w-full">
+        <div className="p-4 w-full max-w-full h-full">
           <div class=" pb-4 text-white ">// Code snippet showcase:</div>
-          <div className="flex flex-col overflow-auto max-h-96 w-full">
+          <div className="flex flex-col overflow-auto  max-h-min w-full">
             {gistData.map((gist: Gist, index) => (
               <div class="py-2">
                 <div className="flex flex-row justify-between">
                   <div className="flex flex-row items-center">
                     <img
                       class="h-12 w-12 rounded-full"
-                      src={gist.owner.avatar_url}
-                    ></img>
+                      src={gist.owner.avatar_url}></img>
                     <div className="p-2">
                       <a
                         className=" text-indigo-500 text-sm font-bold"
                         href={gist.owner.html_url}
-                        target="_blank"
-                      >
+                        target="_blank">
                         @{gist.owner.login}
                       </a>
                       <div className="text-xs text-slate-500">
@@ -106,16 +104,14 @@ export default function CodeSnippets() {
                                 : prevGist.showDescription,
                           }))
                         );
-                      }}
-                    >
+                      }}>
                       <FaComment />
                       <p class=" select-none ">details</p>
                     </div>
                     <a
                       href={gist.html_url}
                       class="flex flex-row items-center text-slate-500 hover:text-slate-400 font-bold gap-2"
-                      target={"_blank"}
-                    >
+                      target={"_blank"}>
                       <AiFillStar />
                       <p>star</p>
                     </a>
@@ -125,8 +121,7 @@ export default function CodeSnippets() {
                 {Object.keys(gist.files).map((file) => (
                   <pre
                     key={file}
-                    class=" overflow-x-auto overflow-y-auto text-sm overflow-hidden "
-                  >
+                    class=" overflow-x-auto overflow-y-auto text-sm overflow-hidden ">
                     <code class="whitespace-pre-wrap xl:whitespace-pre language-typescript bg-slate-950 rounded-2xl shadow border border-slate-800">
                       {gist.files[file].content}
                     </code>
