5e751364f096ef1f0bd6ff3d9bcd36caa89bc036
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2025-03-10 00:24:05 -0600

    updated 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import { VscVscode } from "react-icons/vsc";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex size-full flex-col items-center">
+      <VscVscode className="flex size-full flex-1 text-slate-950/50" />
+      <div className="h-1/2 w-full bg-slate-950 pl-8 pt-8">
+        <h2>404 - Not Found</h2>
+        <p>Could not find requested resource</p>
+        <Link className="hover:underline" href="/">
+          Return Home
+        </Link>
+      </div>
     </div>
   );
 }
